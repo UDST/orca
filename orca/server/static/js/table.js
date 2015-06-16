3f0e553d30976fadb42e4deee99f499834e8f198
Matt Davis <jiffyclub@gmail.com>
2015-06-16 09:50:48 -0700

    make individual buttons in table view into components
    That makes it possible for the individual button to call
    the parent click handler passing in data about the button.

--- orca/server/static/js/table.js
+++ orca/server/static/js/table.js
@@ -1,22 +1,45 @@
 var React = require('react');
+var classNames = require('classnames');
 var _ = require('lodash');
 var dtopts = require('./dtopts.js');
 
 
+var TableButton = React.createClass({
+  clickHandler: function(event) {
+    this.props.clickHandler(this.props.view);
+  },
+  render: function() {
+    var classes = classNames({
+      "btn": true,
+      "btn-default": true,
+      "active": this.props.active
+    });
+
+    return (
+      <button
+          type="button"
+          className={classes}
+          onClick={this.clickHandler}
+          data-view={this.props.view}>
+        {this.props.text}
+      </button>
+    );
+  }
+});
+
 var TableButtons = React.createClass({
   render: function() {
-    var buttons = [['Preview', 'preview']];
-    var button_els = _.map(buttons, function(b, i) {
-        var classes = "btn btn-default";
-        if (i === 0) {
-          classes += " active";
-        }
+    var button_els = _.map(this.props.buttons, function(b) {
       return (
-        <button type="button" className={classes} key={b[1]}>
-          {b[0]}
-        </button>
+        <TableButton
+          text={b[0]}
+          view={b[1]}
+          key={b[1]}
+          active={b[1] === this.props.view ? true : false}
+          clickHandler={this.props.clickHandler}
+        />
       );
-    });
+    }.bind(this));
 
     return (
       <div className="tableButtons btn-group" role="group">
@@ -42,15 +65,42 @@ var TablePreview = React.createClass({
   }
 });
 
-var TableApp = React.createClass({
+var TableDefinition = React.createClass({
   render: function() {
+    return (
+      <div><p>{this.props.table}</p></div>
+    );
+  }
+});
+
+var TableApp = React.createClass({
+  tableButtons: [['Preview', 'preview'], ['Definition', 'definition']],
+  getInitialState: function() {
+    return {view: this.tableButtons[0][1]};
+  },
+  handleButtonClick: function(view) {
+    if (view !== this.state.view) {
+      this.setState({view: view});
+    }
+  },
+  render: function() {
+    var view = this.state.view;
+    if (view === 'preview') {
+      var tableComponent = <TablePreview table={this.props.table} />;
+    } else if (view === 'definition') {
+      var tableComponent = <TableDefinition table={this.props.table} />;
+    }
+
     return (
       <div className="tableApp">
         <div className="page-header">
           <h1>{this.props.table}</h1>
         </div>
-        <TableButtons />
-        <TablePreview table={this.props.table} />
+        <TableButtons
+            buttons={this.tableButtons}
+            view={this.state.view}
+            clickHandler={this.handleButtonClick} />
+        {tableComponent}
       </div>
     );
   }
